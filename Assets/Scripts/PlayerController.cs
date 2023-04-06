90df93b0d193c0299185d8a57001dcf8d273bf6b
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2023-04-06 15:45:42 -0700

    Camera and Player setup
    set up player movement and camera controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     // the rigidbody component
     Rigidbody rigidbody;
+    // if the player is grounded they can jump
+    public bool isGrounded = false;
+    // bool variable that tells fixed update when to jump
+    private bool jump = false;
+    //a float value that controls how high the player jumps
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +25,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movement();
+
+        // fires a ray from the center of the player downwards 1.55 meters
+        // returns true if it hits something
+        // returns False if it doesnt hit anything
+        if (Physics.Raycast(transform.position, Vector3.down, 1.55f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        // if the player presses down the space bar then jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // rule of thumb - do physics manipulation in fixed update instead of update
+        // if jump is true then jump once
+        if (jump)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // sets jump to false after adding force so that jump does happen constantly
+            jump = false;
+        }
     }
 
     private void movement()
@@ -26,25 +62,25 @@ public class PlayerController : MonoBehaviour
         // Moves the Player to the left if the player presses down the "A" key
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            transform.position += transform.right * moveSpeed * Time.deltaTime * -1;
         }
 
         // Moves the Player to the right if the player presses down the "D" key
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
         }
 
         // Moves the Player to the left if the player presses down the "A" key
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+            transform.position += transform.forward * moveSpeed * Time.deltaTime * -1;
         }
 
         // Moves the Player to the right if the player presses down the "D" key
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
     }
 }
